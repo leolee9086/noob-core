3c4ea90ad9db3478ebab0bcfbd38b6f2bb3a098e
leolee9086 <464803712@qq.com>
2023-03-24 02:06:32 +0800

    插件类内置buildSelefSwtich方法

--- frontEnd/noobApi/index.js
+++ frontEnd/noobApi/index.js
@@ -1,8 +1,12 @@
+
 export { Plugin as Plugin } from "./plugin.js";
 export { api as frontEndApi } from "./api.js";
 export { api as noobApi } from "./api.js";
-
 export { kernelApi as kernelApi } from "./api.js";
+export {kernelApi as backEndAPI } from "./api.js"
+
+
+
 export {FormItem as FormItem} from './siyuanUI/form.js'
 export function batchSetAttribute(element, attributes) {
   Object.getOwnPropertyNames(attributes).forEach((name) => {
